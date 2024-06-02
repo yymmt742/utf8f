514f2127010c752b3280c019f14cca3be3ed682f
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-06-02 13:08:53 +0900

    [update] test_codepoint.f90

--- test/test_codepoint.f90
+++ test/test_codepoint.f90
@@ -1,9 +1,19 @@
 program main
   use mod_utf8f
   implicit none
-  if (utf8f_codepoint('a') /= INT(z"61")) ERROR stop
-  if (utf8f_codepoint('あ') /= INT(z"3042")) ERROR stop
-  if (utf8f_codepoint('★') /= INT(z"2605")) ERROR stop
-  if (utf8f_codepoint('💯') /= INT(z"1F4AF")) ERROR stop
+  character(512) :: path, line
+  integer        :: u, ios, id, codepoint
+  call GET_COMMAND_ARGUMENT(1, path)
+  open (NEWUNIT=u, FILE=path)
+  do
+    read (u, '(A)', IOSTAT=ios) line
+    if (ios > 0) error stop
+    if (ios < 0) exit
+    id = INDEX(line, ' ')
+    read (line(:id), *) codepoint
+    if (utf8f_codepoint(line(id + 1:)) /= codepoint) then
+      error stop
+    end if
+  end do
 end program main
 
