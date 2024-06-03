dc7fea50fb7e1dd4ee6ebf7279c73d295f156404
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-06-03 17:43:47 +0900

    [add] binary_search.h

--- src/mod_utf8f_width.f90
+++ src/mod_utf8f_width.f90
@@ -49,30 +49,7 @@ contains
     include "codepoint.h"
   end function codepoint
 !
-  pure recursive subroutine binary_search(codepoint, l, u, n_table, index_table, res)
-    integer, intent(in)      :: codepoint
-    integer, intent(in)      :: l
-    integer, intent(in)      :: u
-    integer, intent(in)      :: n_table
-    integer, intent(in)      :: index_table(n_table)
-    integer, intent(inout)   :: res
-    if (l == u) return
-    res = l + (u - l + 1) / 2
-    if (codepoint < index_table(res)) then
-      block
-        integer :: ul
-        res = res - 1
-        ul = res
-        call binary_search(codepoint, l, ul, n_table, index_table, res)
-      end block
-    else
-      block
-        integer :: ll
-        ll = res
-        call binary_search(codepoint, ll, u, n_table, index_table, res)
-      end block
-    end if
-  end subroutine binary_search
+  include "binary_search.h"
 !
 end submodule mod_utf8f_width
 
